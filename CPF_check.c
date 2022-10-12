07b05d7ece857f14caecd4603f52dd9afbeb4d4d
Leonardo Silva <lpaulino.silva@gmail.com>
2022-10-12 20:25:37 +0000

    Create CPF_check.c
    initial commit;

---
+++ CPF_check.c
@@ -0,0 +1,68 @@
+#include<stdio.h>
+
+int  main()
+{
+  long long int cpf,d1,d2,cont,n9,s,aux,n,dc1,dc2;
+ 
+  printf("Digite o CPF...: ");
+  scanf("%lld",&cpf);
+  
+  d2=(cpf%10);
+  d1=((cpf/10)%10);
+  n9=((cpf/10)/10);
+  
+  aux=n9; 
+  cont=2;
+  s=0;
+  
+  for(;aux!=0;cont++)
+    {
+      n=(aux%10);
+      s=(s+cont * n);
+      aux=(aux/10);
+    }
+
+  s=(s*10);
+  dc1=(s%11);
+  
+  if(dc1==10)
+    {
+      dc1=0;
+    }
+  if(dc1 == d1)
+    { 
+  aux=n9*10+dc1;
+  cont=2;
+  s=0;
+ 
+  for(;aux!=0;cont++)
+    {
+      n=(aux%10);
+      s=(s+cont* n);
+      aux=(aux/10);
+    }
+ 
+  s=(s*10);
+  dc2=(s%11);
+  
+  if(dc2==10)
+    {
+      (dc2=0);
+    }
+
+  if(dc2==d2) 
+    {
+      printf("CPF Valido");
+    }
+  else 
+    { 
+      printf("CPF Invalido");
+    }
+    }  
+  else 
+    {
+      printf("CPF Invalido");
+    } 
+  getch();
+  return 0;
+}
